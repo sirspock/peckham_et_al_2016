b6ab99ab63fe8885255c317997a8e059db5ca9be
mcflugen <huttone@colorado.edu>
2015-10-04 11:15:46 -0600

    Optionally run a user defined set of experiment(s).

--- run_experiments.sh
+++ run_experiments.sh
@@ -13,24 +13,29 @@ if [[ $PBS_O_WORKDIR ]]; then
   cd $PBS_O_WORKDIR
 fi
 
-PYTHON_ROOT=/usr/local/anaconda/2.7
+PYTHON_ROOT=/home/huttone/anaconda
 DAKOTA_ROOT=/usr/local/dakota
 
 export PATH=$(pwd):$PYTHON_ROOT/bin:$DAKOTA_ROOT/bin:$PATH
 export LD_LIBRARY_PATH=$DAKOTA_ROOT/bin:$DAKOTA_ROOT/lib:$LD_LIBRARY_PATH
 
-EXPERIMENT_NAMES="log_model_global \
-  peckham_model \
-  power_model_local \
-  log_model \
-  power_model_global \
-  log_model_local \
-  power_model"
+if [ -z ${EXPERIMENT_NAMES+x} ]; then
+  EXPERIMENT_NAMES="log_model_global \
+    peckham_model \
+    power_model_local \
+    log_model \
+    power_model_global \
+    log_model_local \
+    power_model"
+fi
+
+echo "Running the following set of experiments:"
+echo $EXPERIMENT_NAMES
 
 echo "dakota_version: $(dakota --version)"
 echo "which_dakota: $(which dakota)"
 
-mkdir _experiments && cd _experiments
+mkdir -p _experiments && cd _experiments
 for name in $EXPERIMENT_NAMES; do
   (run_experiment $name > /dev/null) &
 done
