01a6cb39dc0288bfde98699f60b224d68dd6517f
mcflugen <huttone@colorado.edu>
2015-10-06 19:23:02 -0600

    Use a group name for experiments.

--- submit_all_experiments.sh
+++ submit_all_experiments.sh
@@ -1,9 +1,25 @@
 #! /bin/bash
 
-ALL_EXPERIMENTS="log_model_global peckham_model power_model_local log_model power_model_global log_model_local power_model"
+GROUP_NAME="nlsq5"
+ALL_EXPERIMENTS=" \
+  power_model \
+  power_model_nl2sol \
+"
 
-cd _experiments_new
+#  power_model_global \
+#  power_model_cobyla \
+#  power_model_local \
+#  peckham_model_nl2sol \
+#  peckham_model
+#  peckham_model_cobyla \
+#  peckham_model_global \
+#  peckham_model_local \
+#  power_model
+#  power_model_nl2sol
+
+
+cd _experiments_$GROUP_NAME
 for name in $ALL_EXPERIMENTS; do
   export EXPERIMENT_NAMES=$name
-  qsub ../run_experiments.sh -N "dakota-$name-new" -v EXPERIMENT_NAMES -l nodes=1:ppn=1
+  qsub ../run_experiments.sh -N "dakota-$name-$GROUP_NAME" -v EXPERIMENT_NAMES -l nodes=1:ppn=1
 done
