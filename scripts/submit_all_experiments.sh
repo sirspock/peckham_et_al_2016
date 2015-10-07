d6819b2dd260a1dad16b12c38d25cd1a93ba2b72
mcflugen <huttone@colorado.edu>
2015-10-07 09:05:37 -0600

    Adjust paths for new output folder.

--- scripts/submit_all_experiments.sh
+++ scripts/submit_all_experiments.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
-GROUP_NAME="nlsq5"
+GROUP_NAME="power_analytic"
 ALL_EXPERIMENTS=" \
-  power_model \
-  power_model_nl2sol \
+  power_model_cobyla \
+  power_model_global \
+  power_model_local \
 "
 
+#  power_model_nl2sol_numerical_grads \
+#  power_model_numerical_grads \
+#  power_model_cobyla
+#  power_model
+#  power_model_nl2sol_analytic_grads
+#  power_model_numerical_grads
+#  power_model_global
+#  power_model_local
+#  power_model_nl2sol_numerical_grads
 #  power_model_global \
 #  power_model_cobyla \
 #  power_model_local \
@@ -21,5 +31,5 @@ ALL_EXPERIMENTS=" \
 cd output/_experiments_$GROUP_NAME
 for name in $ALL_EXPERIMENTS; do
   export EXPERIMENT_NAMES=$name
-  qsub ../scripts/run_experiments.sh -N "dakota-$name-$GROUP_NAME" -v EXPERIMENT_NAMES -l nodes=1:ppn=1
+  qsub ../../scripts/run_experiments.sh -N "dakota-$name-$GROUP_NAME" -v EXPERIMENT_NAMES -l nodes=1:ppn=1
 done
